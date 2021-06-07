a3da6415acb3d5fc44d847da56f23274fb43977d
Branden <brandenehman45@gmail.com>
2021-06-07 11:57:14 -0600

    Got some vairables and code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementsByTagName("form");
+  const inputs = document.querySelectorAll("input");
+  const description = inputs[0];
+  const submit = inputs[1];
+
+  function newTask() {
+    let task = document.createElement("li");
+    const taskDescription = description.value;
+    const text = document.createTextNode(taskDescription)
+    task.appendChild(text)
+    if (taskDescription === "") {
+      alert("Nothing was written!");
+    } else {
+      document.getElementById("tasks").appendChild(task)
+    }
+    
+  }
 });
