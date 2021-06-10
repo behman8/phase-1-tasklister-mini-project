caa70b15c83f1f7f92e3a38014248b5d6d170817
Branden <brandenehman45@gmail.com>
2021-06-10 17:31:05 -0600

    Completed Lab

--- src/index.js
+++ src/index.js
@@ -1,19 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const form = document.getElementsByTagName("form");
   const inputs = document.querySelectorAll("input");
   const description = inputs[0];
   const submit = inputs[1];
 
+  submit.addEventListener('click', function(e) {
+    e.preventDefault();
+    newTask();
+  });
+
   function newTask() {
     let task = document.createElement("li");
     const taskDescription = description.value;
-    const text = document.createTextNode(taskDescription)
-    task.appendChild(text)
+    const text = document.createTextNode(taskDescription);
+    const removeTask = document.createElement("button");
+    removeTask.addEventListener('click', function(e) {
+      task.remove(e);
+    });
+    removeTask.innerText = "X";
+    removeTask.className = "remove";
+    task.appendChild(text);
+    task.appendChild(removeTask);
     if (taskDescription === "") {
-      alert("Nothing was written!");
+      alert("Nothing was written.");
     } else {
       document.getElementById("tasks").appendChild(task)
     }
+    taskDescription = "";
     
   }
 });
+
